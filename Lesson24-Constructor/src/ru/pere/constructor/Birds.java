8fa14c89f831b143b8cd31c32b9547e73185c284
alexp <alexp@ncc.snt.ru>
2015-03-10 16:27:58 +0600

    Tutorial 24

---
+++ Lesson24-Constructor/src/ru/pere/constructor/Birds.java
@@ -0,0 +1,19 @@
+package ru.pere.constructor;
+
+public class Birds {
+	private String name;
+	private boolean canFly;
+	
+	public Birds(String name, boolean canFly){
+		this.name=name;
+		this.canFly=canFly;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public boolean isCanFly() {
+		return canFly;
+	}
+
+}
---
+++ Lesson24-Constructor/src/ru/pere/constructor/Main.java
@@ -0,0 +1,13 @@
+package ru.pere.constructor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Birds bird=new Birds("Синица", true);
+		
+		System.out.println(bird.getName());
+		System.out.println(bird.isCanFly());
+	}
+
+}
