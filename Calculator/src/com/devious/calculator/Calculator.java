bf3f4d0e7fb07260b01348996181fcc7a879e1ce
alexp <alexp@ncc.snt.ru>
2015-03-12 15:30:54 +0600

    Calculator

---
+++ Calculator/src/com/devious/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.devious.calculator;
+ 
+import javax.swing.*;
+ 
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {}
+ 
+        JFrame calcFrame = new JFrame("Calculator");
+       
+        CalcMode model = new CalcMode();        
+        CalcPanel calcPanel = new CalcPanel(model);
+        calcPanel.init();
+                
+        calcFrame.setContentPane(calcPanel);
+        calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calcFrame.pack();
+        calcFrame.setVisible(true);
+        calcFrame.
+    }
+}
